e1a4508be0e9c46c71aa843c0cb5fee0213956b1
xiaoshunshi <song159769@163.com>
2022-08-03 18:27:52 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,12 +1,12 @@
 export default {
     title: "小顺石的VitePress博客",
     description: "Just playing around.",
+    base: '/vitepress-docs',
     themeConfig: {
         // siteTitle: "My Custom Title", // 可设置为false
         lastUpdated: 'Last Updated',
         logo:'/assect/logo.jpeg',
         outlineTitle:' ',
-        base: '/vitepress-docs',
         nav: [{
                 text: '指南',
                 link: '/guide/installation',
